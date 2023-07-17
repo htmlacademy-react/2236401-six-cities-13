5c1e270c9e35bf5a64663bdbe6d6f6cb9d1f1a1f
Yana Temirgalieva <kolesiko.yana@gmail.com>
2023-07-17 19:04:31 +0300

    makes the stars whole

--- src/utils.ts
+++ src/utils.ts
@@ -14,8 +14,24 @@ function getCapitalLetter(str: string): string {
 
 function getPercent(number: number): string {
 
-  const percent = number * 100 / MAX_STARS_COUNT;
-  return `${percent.toFixed(2)}%`;
+  const percent = Math.ceil(number * 100 / MAX_STARS_COUNT);
+  if (percent <= 20) {
+    return `${20}%`;
+  }
+
+  if (21 > percent && percent <= 40) {
+    return `${40}%`;
+  }
+
+  if (41 > percent && percent <= 60) {
+    return `${60}%`;
+  }
+
+  if (61 > percent && percent <= 80) {
+    return `${80}%`;
+  }
+
+  return `${100}%`;
 }
 
 
