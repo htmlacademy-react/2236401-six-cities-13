4deacacec85c48e84f9479871a3aa051f31c768a
Yana Temirgalieva <kolesiko.yana@gmail.com>
2023-07-19 12:26:14 +0300

    adds the class for the favorites button

--- src/pages/offer-screen/offer-screen.tsx
+++ src/pages/offer-screen/offer-screen.tsx
@@ -26,7 +26,7 @@ function OfferScreen({offers, reviews}: OfferScreenProps): JSX.Element {
   if(!currentOffer) {
     return <Navigate to='/404' />;
   }
-  const {isPremium, title, rating, price, images, type, bedrooms, maxAdults, goods, host, description} = currentOffer;
+  const {isPremium, title, rating, price, images, type, bedrooms, maxAdults, goods, host, description, isFavorite} = currentOffer;
   const {isPro, name, avatarUrl} = host;
 
   const neighbourhoodOffers = offers.filter((offer) =>
@@ -62,7 +62,7 @@ function OfferScreen({offers, reviews}: OfferScreenProps): JSX.Element {
               <h1 className="offer__name">
                 {title}
               </h1>
-              <button className="offer__bookmark-button button" type="button">
+              <button className={classNames({'offer__bookmark-button--active': isFavorite}, 'offer__bookmark-button', 'button')} type="button">
                 <svg className="offer__bookmark-icon" width={31} height={33}>
                   <use xlinkHref="#icon-bookmark"></use>
                 </svg>
